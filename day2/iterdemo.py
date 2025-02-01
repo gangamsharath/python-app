780582ae07f4f99c9bc5cefa4b9ac25909a9d544
gangamsharath <sharathkumar87@gmail.com>
2025-02-01 10:43:13 +0530

    test

---
+++ day2/iterdemo.py
@@ -0,0 +1,18 @@
+#iterator design pattern
+
+data=[10,20,30,40]
+# print(data[2])
+# for i in data:
+#     print(i) #build in iterator
+
+# d=iter(data)
+# print(type(d))
+# print(d.__next__())
+# print(d.__next__())
+# print(next(d))
+# print(next(d))
+# print(next(d))
+
+#panda.head() give by default 5 of top in a file
+#panda.tail() give by default 5 of bottom in a file
+#now how to create your custom iterator
