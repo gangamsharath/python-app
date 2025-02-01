06601cae34c74dc945622d7fb5ceed24c5a8bd0d
gangamsharath <sharathkumar87@gmail.com>
2025-02-01 11:15:21 +0530

    new code

--- day2/oops.py
+++ day2/oops.py
@@ -1,3 +1,20 @@
 #oops module
 
-class Employee
+#class Employee() #by default its object
+class Employee(object):
+     #constructor
+     def __init__(self,empno,ename,salary):
+          self.name=ename #instance variables/properties
+          self.eno=empno
+          self.pay=salary
+
+     def showDetails(self):
+         print('Empno',self.eno)
+         print('Emp name',self.name)
+         print('Salary',self.pay)
+     
+if __name__=='__main__':     
+  e1=Employee(101,'sharath',50000000)
+  e1.showDetails()
+  e2=Employee(102,'radhya',50000000)
+  e2.showDetails()
