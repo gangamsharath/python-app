c44dd867b3baebc2d7b15857efae85ff74fb5de6
gangamsharath <sharathkumar87@gmail.com>
2025-02-02 11:38:09 +0530

    seilization

--- day3/web/serdemo.py
+++ day3/web/serdemo.py
@@ -1,2 +1,20 @@
 #Serialization
-import requests
+import requests,json
+
+url="https://jsonplaceholder.typicode.com/users"
+response=requests.get(url)
+users=json.loads(response.text)
+#print(users)
+data=json.dumps(users,indent=2) #memory level serilization
+#print(data)
+
+#file level serilization
+with open('users.json','w') as fw:
+    json.dump(data,fw)
+    print("file created")
+
+#deserilization
+with open("users.json",'r') as fr:
+    data=json.load(fr)
+    print(data)
+
