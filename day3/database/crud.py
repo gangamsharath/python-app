0818b394f7575d0c229712eb262c78d831e1afcc
gangamsharath <sharathkumar87@gmail.com>
2025-02-02 15:11:40 +0530

    new

--- day3/database/crud.py
+++ day3/database/crud.py
@@ -8,23 +8,56 @@ def connectDB():
     global conn
     conn=sqlite3.connect("employes.db")
     global cursor
-    cursor=conn.cursor
+    cursor=conn.cursor()
     print("connected to database")
 
-def connectDB():
-    pass
 def createTable():
-    pass
+    try:
+        cmd="CREATE TABLE emps(fname text,lname text,pay integer)"
+        cursor.execute(cmd)
+        print("emps table is created")
+    except Exception as ex:
+        print("cannot create table",ex)
+
 def insertEmp(emp):
-    pass
-def readEmp(emp):
-    pass
+    try:
+        cmd="INSERT INTO emps(fname,lname,pay) VALUES(?,?,?)"
+        values=(emp.fname,emp.lname,emp.pay)
+        cursor.execute(cmd,values)
+        cursor.execute("commit")
+        print("employee added to table")
+    except Exception as ex:
+        print("error in inserting to table",ex)
+
+def readEmps():       
+    cursor.execute("SELECT * FROM emps")
+    return cursor.fetchall()
+
 def deleteEmp(emp):
-    pass
+    try:
+        cursor.execute("DELETE FROM emps WHERE :lname = lname",{'lname':emp.lname})
+        cursor.execute('commit')
+    except Exception as ex:
+        print(ex)
 
 #test code
 print("working with sqlite3 database")
 #connect to db
 connectDB()
+#createTable()
+#emp1=Employee('sharath','kumar',50000)
+#emp2=Employee('uma','shankar',50000)
+#insertEmp(emp1)
+#insertEmp(emp2)
+
+data=readEmps()
+print(data)
+
+print("After deleting")
+emp1=Employee('sharath','kumar',50000)
+deleteEmp(emp1)
+
+cursor.close()
+conn.close()
 
 
--- day3/database/employee.py
+++ day3/database/employee.py
@@ -1,6 +1,6 @@
 class Employee(object):
     def __init__(self, fname, lname, pay):
         self.fname = fname
-        self.flname = lname
+        self.lname = lname
         self.pay = pay
 
